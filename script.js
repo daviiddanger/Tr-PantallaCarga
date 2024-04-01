0bc4e93fbf62979842bc559bfacc6c20dfd797c1
Ing. David Morales Juarez <94495486+daviiddanger@users.noreply.github.com>
2024-04-01 13:13:23 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const loadText = document.querySelector('.loading-text');
+const background = document.querySelector('.bg');
+
+let load = 0;
+
+let interval = setInterval(blurring,30);
+
+function blurring(){
+    load++
+    if(load>99){
+        clearInterval(interval)
+    }
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1 , 0);
+    background.style.filter = `blur(${scale(load, 0, 100, 40, 0)}px)`
+}
+
+function scale ( number, inMin, inMax, outMin, outMax){
+    return ( number-inMin)*(outMax-outMin)/(inMax-inMin)+outMin;
+}
